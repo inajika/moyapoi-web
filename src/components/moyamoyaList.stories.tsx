b231e35b91a34b8aba4b5f0cc666b46ce04688e3
inajika <inajika@gmail.com>
2025-01-26 02:00:54 +0900

    chore: 時間を0埋め表示とする

--- src/components/moyamoyaList.stories.tsx
+++ src/components/moyamoyaList.stories.tsx
@@ -1,22 +1,33 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import MoyamoyaList from './moyamoyaList';
 
+import { DATE_LOCALE_OPTIONS } from '@/lib/settings';
+
 // サンプルもやもやデータ
 const moyamoyaList = [
   {
     id: '12345',
     content: 'もやもや内容1',
-    date: '2024-12-27',
+    date: new Date('2024-12-24 01:02:03').toLocaleString(
+      'ja-JP',
+      DATE_LOCALE_OPTIONS,
+    ),
   },
   {
     id: '23456',
     content: 'もやもや内容2',
-    date: '2024-12-26',
+    date: new Date('2024-12-25 01:02:03').toLocaleString(
+      'ja-JP',
+      DATE_LOCALE_OPTIONS,
+    ),
   },
   {
     id: '34567',
     content: 'もやもや内容3',
-    date: '2024-12-25',
+    date: new Date('2024-12-26 01:02:03').toLocaleString(
+      'ja-JP',
+      DATE_LOCALE_OPTIONS,
+    ),
   },
 ];
 
